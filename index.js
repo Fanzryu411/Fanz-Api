8cc5ea3d2bf7b430f5d8d774fc58d88bbdc76284
Fanzryu411 <118971536+Fanzryu411@users.noreply.github.com>
2024-11-23 15:52:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 var express = require('express'),
     cors = require('cors'),
     secure = require('ssl-express-www');
-const PORT = process.env.PORT || 8080 || 5000 || 3000;
-var { color } = require('./lib/color.js');
+const PORT = process.env.PORT || 8080 || 5000 || 3000
+var { color } = require('./lib/color.js')
 
 var mainrouter = require('./routes/main'),
-    apirouter = require('./routes/api');
+    apirouter = require('./routes/api')
 
-var app = express();
+var app = express()
 app.enable('trust proxy');
-app.set("json spaces", 2);
-app.use(cors());
-app.use(secure);
+app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
+app.use(express.static("public"))
 
-// Menyajikan file statis dari folder 'views'
-app.use(express.static("views"));  // Menyajikan file statis dari folder views
-
-// Menambahkan route untuk mengakses index.html di folder views
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: './views' });  // Pastikan file index.html ada di folder views
-});
-
-app.use('/api', apirouter);
+app.use('/', mainrouter)
+app.use('/api', apirouter)
 
 app.listen(PORT, () => {
-    console.log(color("Server running on port " + PORT, 'green'));
-});
+    console.log(color("Server running on port " + PORT,'green'))
+})
 
-module.exports = app;
+module.exports = app
