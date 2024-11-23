06a196778d8c1ab39053c203ace59605b4810207
Fanzryu411 <118971536+Fanzryu411@users.noreply.github.com>
2024-11-23 07:08:16 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = '';
+var url = 'https://fanz-lqcw0a93k-fanz1234s-projects.vercel.app/';
 try {
 if(url == '') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
