a38c5c9f20fb65697ca0811e7680fe18d036f3fc
Fanzryu411 <118971536+Fanzryu411@users.noreply.github.com>
2024-11-23 16:33:23 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,17 @@
-const app = require("../index");
-module.exports = app;
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Menyajikan file statis dari folder views
+app.use(express.static(path.join(__dirname, "views")));
+
+// Rute untuk mengarahkan ke index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+// Menjalankan server pada port 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
