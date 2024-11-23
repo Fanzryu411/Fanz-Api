d33ff209ed17a1b26d231472541616de9ab8fd84
Fanzryu411 <118971536+Fanzryu411@users.noreply.github.com>
2024-11-23 10:07:20 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,24 +1,24 @@
 var express = require('express'),
     cors = require('cors'),
     secure = require('ssl-express-www');
-const PORT = process.env.PORT || 8080 || 5000 || 3000;
-var { color } = require('./lib/color.js');
+const PORT = process.env.PORT || 8080 || 5000 || 3000
+var { color } = require('./lib/color.js')
 
 var mainrouter = require('./routes/main'),
-    apirouter = require('./routes/api');
+    apirouter = require('./routes/api')
 
-var app = express();
+var app = express()
 app.enable('trust proxy');
-app.set("json spaces", 2);
-app.use(cors());
-app.use(secure);
-app.use(express.static("public"));
+app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
+app.use(express.static("public"))
 
-// Menambahkan router
-app.use('/', mainrouter);
-app.use('/api', apirouter);
+app.use('/', mainrouter)
+app.use('/api', apirouter)
 
-// Mengekspor aplikasi untuk dijalankan sebagai fungsi serverless di Vercel
-module.exports = (req, res) => {
-    app(req, res);  // Menjalankan aplikasi Express dengan req dan res yang diteruskan
-};
+app.listen(PORT, () => {
+    console.log(color("Server running on port " + PORT,'green'))
+})
+
+module.exports = app
